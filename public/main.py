30a2245b7e1deda58f844dfa32cee45b1ad18844
levi2k6 <kogmawa@gmail.com>
2025-01-21 16:59:47 +0800

    python update

---
+++ public/main.py
@@ -0,0 +1,14 @@
+from module import add, minus, multiply
+
+
+
+def main():
+    add(20, 100)
+    add(200, 200)
+    minus(1000, 500)
+
+
+
+
+# /////////////////////////////////////////////////////////
+main()
---
+++ public/module.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    c = a + b
+    print(c)
+
+
+def minus(a, b):
+    c = a - b
+    print(c)
+
+
+def multiply(a, b):
+    c = a * b
+    print(c)
